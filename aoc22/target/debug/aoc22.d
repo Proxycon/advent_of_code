0ba504605af78840fb9ffe522ff168bcc1d7e6aa
Leon Rohmann <leon.rohmann@campus.tu-berlin.de>
2023-07-31 19:07:36 +0200

    deleting target/

--- aoc22/target/debug/aoc22.d
+++
@@ -1,1 +0,0 @@
-/home/proxycon/Documents/projects/advent_of_code/aoc22/target/debug/aoc22: /home/proxycon/Documents/projects/advent_of_code/aoc22/src/main.rs
--- aoc22/target/debug/deps/aoc22-5fcb5fd1365d1b24.d
+++
@@ -1,5 +0,0 @@
-/home/proxycon/Documents/projects/advent_of_code/advent_of_code/aoc22/target/debug/deps/aoc22-5fcb5fd1365d1b24.rmeta: src/main.rs
-
-/home/proxycon/Documents/projects/advent_of_code/advent_of_code/aoc22/target/debug/deps/aoc22-5fcb5fd1365d1b24.d: src/main.rs
-
-src/main.rs:
