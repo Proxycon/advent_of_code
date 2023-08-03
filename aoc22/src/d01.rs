c90941eea784b249ca0d0091809076b8f4bc84f3
Leon Rohmann <leon.rohmann@campus.tu-berlin.de>
2023-08-03 23:27:50 +0200

    clippy fixes

--- aoc22/src/d01.rs
+++ aoc22/src/d01.rs
@@ -2,13 +2,13 @@ use std::{cmp, fs::read_to_string};
 
 pub fn d01(input: &str) {
     let file = read_to_string(input).unwrap();
-    let mut lines = file.lines();
+    let lines = file.lines();
 
     let mut max = 0;
     let mut current = 0;
 
-    while let Some(line) = lines.next() {
-        if line.len() == 0 {
+    for line in lines {
+        if line.is_empty() {
             max = cmp::max(max, current);
             current = 0;
         } else {
@@ -22,13 +22,13 @@ pub fn d01(input: &str) {
 
 pub fn d01_2(input: &str) {
     let file = read_to_string(input).unwrap();
-    let mut lines = file.lines();
+    let lines = file.lines();
 
     let mut elfs: Vec<u32> = Vec::new();
     let mut current: u32 = 0;
 
-    while let Some(line) = lines.next() {
-        if line.len() == 0 {
+    for line in lines {
+        if line.is_empty() {
             elfs.push(current);
             current = 0;
         } else {
@@ -39,5 +39,5 @@ pub fn d01_2(input: &str) {
     elfs.sort();
     elfs.reverse();
     println!("#d01 - 2\nelfs: {:#?}", vec![elfs[0], elfs[1], elfs[2]]);
-    println!("calories carried by top 3 elfs: {:#?}", elfs[..3].into_iter().sum::<u32>())
+    println!("calories carried by top 3 elfs: {:#?}", elfs[..3].iter().sum::<u32>())
 }
